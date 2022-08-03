f1d8e4dd0b8cd1059fc222ddc2c5244c925cb5ba
md5dalton <md5daltonggs@gmail.com>
2022-08-02 22:29:25 -0400

    Update index.js

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -32,7 +32,7 @@ export default () => (
             <p>
                 I collaborate with your website and graphic designers to build your website from designs made from various softwares such as Adobe XD,
                 Sketch, Figma, Photoshop, etc, fashioning everything from the landing page to site layout and enhancing user experience. I integrate data
-                from various back-end services whether it's through a REST api or GraphQL, all the way to completion and deployment.
+                from various back-end services whether it's through a REST api, all the way to completion and deployment.
             </p>
             <p>I'll journey with you to some of the projects I've worked on.</p>
             <Button><Link to="/work">See My Work</Link></Button>
