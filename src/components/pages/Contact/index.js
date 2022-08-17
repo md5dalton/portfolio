51e8469d9b4c45251ba105a0a6bd5189f34c942b
md5dalton <md5daltonggs@gmail.com>
2022-08-17 01:14:04 -0400

    build

--- src/components/pages/Contact/index.js
+++ src/components/pages/Contact/index.js
@@ -4,40 +4,57 @@ import Button from '../../UI/Button'
 
 import "./styles.sass"
 
-export default () => (
-    <Page name="contact">
-        <section className="top">
-            <div className="profile"><img src="/images/svg/contact.svg" alt="profile" /></div>
-            <h1>Get in touch with me</h1>
-        </section>
-        <section>
-            <p>
-                It's always a pleasure to help others like you to bring their amazing website designs to life. Here is my email should you 
-                wish to contact me: <a href="mailto:md5daltonggs@gmail.com">md5daltonggs@gmail.com</a>
-            </p>
-        </section>
-        <section>
-            <form>
-                <div className="form-section">
-                    <label>name</label>
-                    <input type="text" />
-                </div>
-                <div className="form-section">
-                    <label>your email</label>
-                    <input type="email" />
-                </div>
-                <div className="form-section">
-                    <label>your message</label>
-                    <textarea></textarea>
-                </div>
-                <div className="form-section">
-                    <Button type="submit" disabled>Send</Button>
-                </div>
-            </form>
-            <p className="side-note">
-                *Please note, this contact form is not functional for the moment, it's just a placeholder. Should you need to contact me,
-                here is my email: <a href="mailto:md5daltonggs@gmail.com">md5daltonggs@gmail.com</a>
-            </p>
-        </section>
-    </Page>        
-)
+export default () => {
+
+    const submitHandler = ev => {
+
+        ev.preventDefault()
+
+        const { name, email, message } = ev.target
+
+        if (!message.value) message.focus()
+        if (!email.value) email.focus()
+        if (!name.value) name.focus()
+
+        if (message.value && email.value && name.value) window.open(`mailto:md5daltonggs@gmail.com?subject=Interested Client: ${name}&body=${message}`)
+
+    }
+    
+    return (
+        <Page name="contact">
+            <section className="top">
+                <div className="profile"><img src="/images/svg/contact.svg" alt="profile" /></div>
+                <h1>Get in touch with me</h1>
+            </section>
+            <section>
+                <p>
+                    It's always a pleasure to help others like you to bring their amazing website designs to life. Here is my email should you 
+                    wish to contact me: <a href="mailto:md5daltonggs@gmail.com">md5daltonggs@gmail.com</a>
+                </p>
+            </section>
+            <section>
+                <form onSubmit={submitHandler}>
+                    <div className="form-section">
+                        <label>name</label>
+                        <input type="text" name="name" />
+                    </div>
+                    <div className="form-section">
+                        <label>your email</label>
+                        <input type="email" name="email" />
+                    </div>
+                    <div className="form-section">
+                        <label>your message</label>
+                        <textarea name="message"></textarea>
+                    </div>
+                    <div className="form-section">
+                        <Button type="submit">Send</Button>
+                    </div>
+                </form>
+                <p className="side-note">
+                    *Please note, this contact form is not functional for the moment, it's just a placeholder. Should you need to contact me,
+                    here is my email: <a href="mailto:md5daltonggs@gmail.com">md5daltonggs@gmail.com</a>
+                </p>
+            </section>
+        </Page>        
+    )
+}
